606affdef0510dd0d7de11b72bd69ec616971b69
Abheeshek Jain <abheedjain@gmail.com>
2023-10-07 07:11:00 +0000

    #26 WIP 1 until create bucket

--- main.tf
+++ main.tf
@@ -1,19 +1,22 @@
-# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
-resource "random_string" "bucket_name" {
-  length  = 16
-  special = false
-  lower   = true
-  upper   = false
-  #   override_special = "!@#$%&*()-_=+[]{}<>:?"
+terraform {
+  #backend "remote" {
+  #  hostname = "app.terraform.io"
+  #  organization = "ExamPro"
+
+  #  workspaces {
+  #    name = "terra-house-1"
+  #  }
+  #}
+  #cloud {
+  #  organization = "Equinox9"
+  #  workspaces {
+  #    name = "terra-house-1"
+  #  }
+  #}
 }
 
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-resource "aws_s3_bucket" "website_bucket" {
-   # Bucket Naming Rules
-  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
-  bucket = var.bucket_name
-
-  tags = {
-    UserUuid = var.user_uuid
-  }
+module "terrahouse" {
+    source = "./modules/terrahouse_aws"
+    user_uuid= var.user_uuid
+    bucket_name = var.bucket_name
 }
