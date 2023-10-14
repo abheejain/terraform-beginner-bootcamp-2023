ad641e6543ed41aaf6e51e54017c2ac370391011
Abheeshek Jain <abheedjain@gmail.com>
2023-10-14 12:56:34 +0000

    #50 Fixed issues with provider, setup two different towns- in missingo

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ provider "terratowns" {
 module "home_thaifood_hosting" {
  source = "./modules/terrahome_aws"
  user_uuid= var.teacherseat_user_uuid
- public_path = var.thaifood_public_path
+ public_path = var.thaifood.public_path
  #index_html_filepath = var.index_html_filepath
  #error_html_filepath = var.error_html_filepath
  content_version = var.thaifood.content_version
@@ -46,28 +46,28 @@ DESCRIPTION
   domain_name = module.home_thaifood_hosting.domain_name
   #domain_name = "3hdq32dgz.cloudfront.net" // change this if you get error in 'resource'
   town = "missingo"
-  content_version = 1
+  content_version = var.thaifood.content_version
 }
 
 
 #Host - 02 
-# module "home_thaiband_hosting" {
-#  source = "./modules/terrahome_aws"
-#  user_uuid= var.teacherseat_user_uuid
-#  public_path = var.thaiband_public_path
-#  #index_html_filepath = var.index_html_filepath
-#  #error_html_filepath = var.error_html_filepath
-#  content_version = var.thaiband.content_version
-#  #assets_path = var.assets_path
-# }
+module "home_thaiband_hosting" {
+ source = "./modules/terrahome_aws"
+ user_uuid= var.teacherseat_user_uuid
+ public_path = var.thaiband.public_path
+ #index_html_filepath = var.index_html_filepath
+ #error_html_filepath = var.error_html_filepath
+ content_version = var.thaiband.content_version
+ #assets_path = var.assets_path
+}
 
-# resource "terratowns_home" "home" {
-#   name = "Roman Anton Band"
-#   description = <<DESCRIPTION
-# Roman Anton brings you art, books, music, and related creations through this one-of-a-kind website. All the creations are originals made by Roman Anton.  The Roman Anton Team invites you to take a look at the many offerings inside
-# DESCRIPTION
-#   domain_name = module.home_thaiband_hosting.cloudfront_url
-#   #domain_name = "3hdq32dgz.cloudfront.net" // change this if you get error in 'resource'
-#   town = "missingo"
-#   content_version = 1
-# }
+resource "terratowns_home" "home_thaiband" {
+  name = "Roman Anton Band"
+  description = <<DESCRIPTION
+Roman Anton brings you art, books, music, and related creations through this one-of-a-kind website. All the creations are originals made by Roman Anton.  The Roman Anton Team invites you to take a look at the many offerings inside
+DESCRIPTION
+  domain_name = module.home_thaiband_hosting.domain_name
+  #domain_name = "3hdq32dgz.cloudfront.net" // change this if you get error in 'resource'
+  town = "missingo"
+  content_version = var.thaiband.content_version
+}
