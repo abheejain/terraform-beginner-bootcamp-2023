44df4965d21a340308581faabe48874eb155c364
Abheeshek Jain <abheedjain@gmail.com>
2023-10-14 07:20:17 +0000

    #48 Finished Terra home testing in the Testing Town missingo

--- main.tf
+++ main.tf
@@ -22,31 +22,31 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = var.terratowns_endpoint
-  user_uuid = var.teacherseat_user_uuid
-  token = var.terratowns_access_token
+  endpoint=var.terratowns_endpoint
+  user_uuid=var.teacherseat_user_uuid
+  token=var.terratowns_access_token
   //endpoint = var.terratowns_endpoint
   //user_uuid = var.teacherseat_user_uuid // Your account uuid 
   //token = var.terratowns_access_token // get from the platform 
 }
 
-#module "terrahouse_aws" {
-#  source = "./modules/terrahouse_aws"
-#  user_uuid= var.teacherseat_user_uuid
-#  bucket_name = var.bucket_name
-#  index_html_filepath = var.index_html_filepath
-#  error_html_filepath = var.error_html_filepath
-#  content_version = var.content_version
-#  assets_path = var.assets_path
-#}
+module "terrahouse_aws" {
+ source = "./modules/terrahouse_aws"
+ user_uuid= var.teacherseat_user_uuid
+ //bucket_name = var.bucket_name
+ index_html_filepath = var.index_html_filepath
+ error_html_filepath = var.error_html_filepath
+ content_version = var.content_version
+ assets_path = var.assets_path
+}
 
 resource "terratowns_home" "home" {
   name = "Thai Vegetarian Food, Aroi "
   description = <<DESCRIPTION
 Thai vegetarian food is a delightful and aromatic cuisine rich in flavors, utilizing ingredients like tofu, vegetables, coconut milk, and Thai spices. Dishes such as Pad Thai, Green Curry, and Som Tum (Green Papaya Salad) are popular options for vegetarians.
 DESCRIPTION
-  #domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "3fdq32dgz.cloudfront.net"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  #domain_name = "3hdq32dgz.cloudfront.net"
   town = "missingo"
   content_version = 1
 }
